4ec6dfa55ef3128edd2c04bc584ae1b60a2c8535
Jarosław Orzeł <jaroslaw.orzel@emplocity.pl>
2022-07-03 22:12:42 +0200

    Mixed logic example

---
+++ mixed.py
@@ -0,0 +1,79 @@
+from flask import Flask, jsonify, request, session
+
+
+app = Flask(__name__)
+
+
+class AuthError(Exception):
+    pass
+
+
+class InvalidEmailDomain(Exception):
+    pass
+
+
+class MaxVoucherException(Exception):
+    pass
+
+
+class Voucher:
+    sender_id: int
+    points: int
+    is_active: bool
+
+
+class Customer:
+    id: int
+    is_vip: bool
+
+
+def get_customer_id_from_token(request):
+    customer_id = ...  # get from token
+    return customer_id
+
+
+def get_customer(request, session):
+    customer_id = get_customer_id_from_token(request)
+    return session.query(Customer).get(customer_id)
+
+
+def send_voucher_mail(email: str) -> None:
+    # send email with voucher
+    return
+
+
+AVAILABLE_DOMAINS = ["test.com", "db.test"]
+MAX_VOUCHERS = 3
+
+
+@app.route("/vouchers", methods=["POST"])
+def send_voucher(self):
+    current_customer = get_customer(request)
+    if not current_customer:
+        raise AuthError()
+
+    email = request.data["email"]
+    if email.split("@")[1] not in AVAILABLE_DOMAINS:
+        raise InvalidEmailDomain()
+
+    if session.query(Voucher).filter_by(sender_id=current_customer.id) >= MAX_VOUCHERS:
+        raise MaxVoucherException()
+
+    voucher_points = 10 if current_customer.is_vip else 3
+    voucher = Voucher(points=voucher_points, sender_id=current_customer.id)
+    session.add(voucher)
+    session.commit()
+
+    send_voucher_mail(email)
+
+@app.route("/vouchers", methods=["GET"])
+def vouchers(self):
+    current_customer = get_customer(request)
+    if not current_customer:
+        raise AuthError()
+
+    is_active = bool(request.args.get('is_active', 0))
+    vouchers = session.query(Voucher).filter_by(sender_id=current_customer.id)
+    if is_active:
+        vouchers = vouchers.filter_by(is_active=is_active)
+    return jsonify([{"is_active": v.is_active, "points": v.points} for v in vouchers])
